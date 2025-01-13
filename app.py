11841585c768f210a900f326ac7d7fac3c6c4d72
Meinhard Kissich <privat@meinhard-kissich.at>
2025-01-13 18:48:12 +0100

    Move configs into sidebar

--- app.py
+++ app.py
@@ -8,89 +8,72 @@ from pathlib import Path
 from celery import Celery
 import pandas as pd
 import numpy as np
-from config import load_config, store_config, config_ui
+from config import Config
 from pipelines import pipeline_from_pdf, pipeline_from_text
 
-config_dict = None
 
-def unique_dir():
-    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")[:-3]
+class App():
+    def __init__(self):
+        self.config = Config(voices_dir=Path("voices").absolute())
 
-def text_tts_start_pipeline():
-    working_dir = Path("data").absolute() / unique_dir()
-    pipeline_from_text(st.session_state.tts_source_text, working_dir, config_dict)
+    def main(self):
+        st.set_page_config(page_title="Paper2Go")
+        st.title("Paper2Go")
+        app = Celery("tasks", broker="redis://localhost:6379/0")
 
-def main():
-    global config_dict
+        tab_pdf, tab_text = st.tabs(["From PDF", "From Text"])
 
-    st.set_page_config(page_title="Paper2Go")
-    st.title("Paper2Go")
-    app = Celery("tasks", broker="redis://localhost:6379/0")
+        with tab_pdf:
+            source_files = st.file_uploader("Select a pdf to convert.", accept_multiple_files=True, type=["pdf"])
+            if st.button("Convert PDFs" if len(source_files) > 1 else "Convert PDF"):
+                working_dir = Path("data").absolute() / self.unique_dir()
+                pipeline_from_pdf(source_files, working_dir, self.config_dict)
 
-    config_dict = load_config('paper2go.ini')
+        with tab_text:
+            source_text = st.text_area("Type your script here", key="tts_source_text", on_change=self.text_tts_start_pipeline)
 
-    tab_pdf, tab_text, tab_settings = st.tabs(["From PDF", "From Text", "Settings"])
 
-    with tab_pdf:
-        source_files = st.file_uploader("Select a pdf to convert.", accept_multiple_files=True, type=["pdf"])
-        if st.button("Convert PDFs" if len(source_files) > 1 else "Convert PDF"):
-            working_dir = Path("data").absolute() / unique_dir()
-            pipeline_from_pdf(source_files, working_dir, config_dict)
-
-    with tab_text:
-        source_text = st.text_area("Type your script here", key="tts_source_text", on_change=text_tts_start_pipeline)
-
-    with tab_settings:
-        col_down, col_restore, col_up = st.columns([2,2,8])
-        with col_down:
-            st.download_button(
-                label="Download",
-                data=store_config(config_dict, stream=True),
-                file_name='config.ini',
-                mime='text/plain'
-            )
-        with col_up:
-            with st.expander("Upload"):
-                uploaded_ini = st.file_uploader(
-                    label="Upload Config INI",
-                    type=["ini"],
-                    label_visibility="hidden")
-                if uploaded_ini is not None:
-                    with open('paper2go.ini', "wb") as f:
-                        f.write(uploaded_ini.getbuffer())
-        with col_restore:
-            if st.button("Restore"):
-                config_dict = load_config('paper2go_defaults.ini')
-
-        config_ui(config_dict)
-       
-    st.sidebar.title("About")
-    st.sidebar.write("Paper2Go converts a PDF to Markdown, re-formulates it, synthesizes it into speech, and converts it to MP3 audio.")        
-    footer()
-
-def footer():
-    st.markdown(
-        f"""
-        <style>
-        .footer {{
-            position: fixed;
-            left: 0;
-            bottom: 0;
-            width: 100%;
-            background-color: #0e1117;
-            color: gray;
-            text-align: center;
-            padding: 10px 0;
-        }}
-        </style>
+        self.config.config_ui()
+            
         
-        <div class="footer">
-            Developed with 🩶 by Meinhard Kissich
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
+        st.sidebar.title("About")
+        st.sidebar.write("Paper2Go converts a PDF to Markdown, re-formulates it, synthesizes it into speech, and converts it to MP3 audio.")        
+        self.footer()
+
+    def unique_dir(self):
+        return datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")[:-3]
+
+    def text_tts_start_pipeline(self):
+        working_dir = Path("data").absolute() / self.unique_dir()
+        pipeline_from_text(st.session_state.tts_source_text, working_dir, self.config.as_dict())
+
+
+    def footer(self):
+        st.markdown(
+            f"""
+            <style>
+            .footer {{
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: #0e1117;
+                color: gray;
+                text-align: center;
+                padding: 10px 0;
+            }}
+            </style>
+            
+            <div class="footer">
+                Developed with 🩶 by Meinhard Kissich
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
 
 
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.main()
+
--- config.py
+++ config.py
@@ -7,61 +7,153 @@ import streamlit as st
 from io import StringIO
 from pathlib import Path
 from pydub import AudioSegment
+import streamlit_nested_layout
+from collections import namedtuple
 
-def load_config(file_path='paper2go.ini'):
-    config = configparser.ConfigParser()
-    config.read(file_path)
-    return {section: dict(config.items(section)) for section in config.sections()}
+class Config:
 
-def store_config(config_dict, file_path=None, stream=False):
-    config = configparser.ConfigParser()
-    for section in config_dict.keys():
-        config.add_section(section)
+    slider_config_setup = namedtuple("SliderConfigSetup", "section key label type min max")
+    checkbox_config_setup = namedtuple("CheckboxConfigSetup", "section key label")
 
-    for section in config_dict.keys():
-        section_dict = config_dict[section]
-        fields = section_dict.keys()
-        for field in fields:
-            value = section_dict[field]
-            config.set(section, field, str(value))
+    config_sliders = { "Fish-Speech" : [slider_config_setup("TTS_FISH", "num_samples", "Num. Samples", int, 1, 10),
+                                        slider_config_setup("TTS_FISH", "top_p", "Top P", float, 0.0, 1.0),
+                                        slider_config_setup("TTS_FISH", "repetition_penalty", "Repetition Penalty", float, 0.0, 5.0),
+                                        slider_config_setup("TTS_FISH", "temperature", "Temperature", float, 0.0, 1.0),
+                                        slider_config_setup("TTS_FISH", "seed", "Seed", int, 1, 1000),
+                                        slider_config_setup("TTS_FISH", "chunk_length", "Chunk Length", int, 1, 1000)],
+                        "XTTSv2" : [slider_config_setup("TTS_XTTSv2", "speed", "Speed", float, 0.1, 4.0)]}
 
-    if file_path is not None:
-        with open(file_path, 'w') as file_path:
-            config.write(file_path)
-
-    if stream:
-        output = StringIO()
-        config.write(output)
-        return output.getvalue()
+    config_checkboxes = {   "Fish-Speech" : [checkbox_config_setup("TTS_FISH", "compile", "Compile")],
+                            "XTTSv2" : [checkbox_config_setup("TTS_XTTSv2", "split_sentences", "Split Sentence")]}
 
 
-def config_ui(config_dict):
-    tts_method = st.radio("Select method for TTS 👇", ["Fish-Speech", "XTTSv2"], index=0)
+    def __init__(self, voices_dir:Path, config_path="paper2go.ini", config_defaults_path="paper2go_defaults.ini"):
+        self.config_path = config_path
+        self.config_defaults_path = config_defaults_path
+        self.voices_dir = voices_dir
+        self.load_config()
 
-    tts_voice_upload = st.file_uploader(
-                    label="Voice sample",
-                    type=["mp3", "wav"])
+    def as_dict(self):
+        return st.session_state["config"]
+        
+    def load_config(self):
+        parser = configparser.ConfigParser()
+        parser.read(self.config_path)
+        st.session_state["config"] = {section: dict(parser.items(section)) for section in parser.sections()}
+
+    def store_config(self, stream=False):
+        parser = configparser.ConfigParser()
+        for section in st.session_state["config"].keys():
+            parser.add_section(section)
+
+        for section in st.session_state["config"].keys():
+            section_dict = st.session_state["config"][section]
+            fields = section_dict.keys()
+            for field in fields:
+                value = section_dict[field]
+                parser.set(section, field, str(value))
+
+        if self.config_path is not None:
+            with open(self.config_path, 'w') as f:
+                parser.write(f)
+
+        if stream:
+            output = StringIO()
+            parser.write(output)
+            return output.getvalue()
+
+    def update_config(self, key, value):
+        if self.config and (key in self.config):
+            self.config[key] = value
+
+    @st.dialog("Upload your INI File")
+    def config_upload_ini_dialog(self):
+        uploaded_ini = st.file_uploader(
+            label="Upload INI",
+            type=["ini"],
+            label_visibility="hidden")
+        if uploaded_ini is not None:
+            with open('paper2go.ini', "wb") as f:
+                f.write(uploaded_ini.getbuffer())
+
+
+    def __tts_translate(self, label_txt):
+        lut = {"Fish-Speech": "TTS_FISH", "XTTSv2": "TTS_XTTSv2"}
+        return lut[label_txt]
     
-    if tts_voice_upload is not None:
-        with open("voices"/tts_voice_upload.name, "wb") as f:
-            f.write(tts_voice_upload.getbuffer())
-            if tts_voice_upload.type != "wav":
-                f = Path("voices"/tts_voice_upload).absolute()
-                AudioSegment.from_wav(f).export(f.with_suffix('.wav'), format="wav")
+    def __clear_voices(self):
+        for filename in os.listdir(self.voices_dir):
+            file_path = self.voices_dir / filename
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.remove(file_path)
 
-    col_clear, col_select = st.columns([2, 12])
-    with col_clear:
-        if st.button("Clear"):
-            for filename in os.listdir("voices"):
-                file_path = os.path.join("voices", filename)
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.remove(file_path)
-    with col_select:
-        tts_voice_select = st.selectbox(
-            "Select Voice to use",
-            ["Default"] + [str(file) for file in Path("voices").rglob('*') if file.is_file()]
-        )
+    def config_ui(self):
+        with st.sidebar:
+            st.title("Settings")
 
-    col_xtts, col_fish = st.columns(2)
+            if st.button("✅ Save", use_container_width=True):
+                for s in sum(Config.config_sliders.values(), []):
+                    st.session_state["config"][s.section][s.key] = str(st.session_state.get(f"{s.section}_{s.key}"))
+                for c in sum(Config.config_checkboxes.values(), []):
+                    st.session_state["config"][c.section][c.key] = str(st.session_state.get(f"{c.section}_{c.key}"))
+                print(st.session_state["config"])
+                self.store_config()
 
-    # TODO: update dict and UI
+            with st.expander("Store / Restore"):
+                if st.button("⚙️ Restore Defaults"):
+                    st.session_state["config"] = self.load_config(self.config_defaults_path)
+
+                st.download_button(
+                    label="💾 Download Config",
+                    data=self.store_config(stream=True),
+                    file_name='config.ini',
+                    mime='text/plain'
+                )
+
+                if "config_upload_ini_dialog" not in st.session_state:
+                    if st.button("📁 Upload Config INI"):
+                        self.config_upload_ini_dialog()
+
+            with st.expander("Basic Settings", expanded=True):
+                st.session_state["config"]["TTS"]["tts_method"] = self.__tts_translate(st.radio("Select method for TTS 👇", ["Fish-Speech", "XTTSv2"], index=0))
+
+                tts_voice_upload = st.file_uploader(
+                                label="Voice sample",
+                                type=["mp3", "wav"])
+            
+                if tts_voice_upload is not None:
+                    with open(self.voices_dir/tts_voice_upload.name, "wb") as f:
+                        f.write(tts_voice_upload.getbuffer())
+                        if tts_voice_upload.type != "wav":
+                            f = Path(self.voices_dir/tts_voice_upload).absolute()
+                            AudioSegment.from_wav(f).export(f.with_suffix('.wav'), format="wav")
+
+                if st.button("Clear Uploaded"):
+                    self.__clear_voices()
+
+                st.session_state["config"]["TTS"]["voice"] = self.voices_dir / st.selectbox("Select Voice to use",
+                    ["Default"] + [str(file.name) for file in Path(self.voices_dir).rglob('*') if file.is_file()]
+                )
+
+            with st.expander("Advanced Settings", expanded=True):
+                for expander in Config.config_sliders.keys():
+                    with st.expander(expander): 
+                        if expander in Config.config_sliders:
+                            for s in Config.config_sliders[expander]:
+                                st.slider(
+                                    label=s.label,
+                                    min_value=s.min,
+                                    max_value=s.max,
+                                    value=s.type(st.session_state["config"][s.section][s.key]),
+                                    key=f"{s.section}_{s.key}"
+                                )
+
+                        if expander in Config.config_checkboxes:
+                            for c in Config.config_checkboxes[expander]:
+                                st.checkbox(
+                                    label=c.label,
+                                    value=bool(st.session_state["config"][c.section][c.key]),
+                                    key=f"{c.section}_{c.key}"
+                                )
+
+        # TODO: update dict and UI
