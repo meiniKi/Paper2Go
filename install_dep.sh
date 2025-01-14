003bd6a1df2d9cc94fe69a6dbb36d8d8cb1f1de2
Meinhard Kissich <privat@meinhard-kissich.at>
2025-01-14 21:02:58 +0100

    Docker support, handle dependencies

---
+++ install_dep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone --branch "v2.4.0" --single-branch https://github.com/rhasspy/gruut gruut
+cd gruut || exit
+sed -i "s/"jsonlines~=1.2.0"/"jsonlines==3.1.0"/g" requirements.txt
+
+pip install .[de,en,es,fr]
+cd ..
+rm -rf gruut
+pip install -r requirements.txt
